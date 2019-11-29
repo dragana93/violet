e7008bace54444597df924d81d99e799697641bd
draganasavanovic <dragana.savanovic@wayseven.com>
2019-11-29 15:42:27 +0100

    simple app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VioletComponent } from './violet/violet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VioletComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/violet/violet.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-violet",
+  templateUrl: "./violet.component.html",
+  styleUrls: ["./violet.component.css"]
+})
+export class VioletComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  yes() {
+    console.log("YES");
+  }
+  no() {
+    console.log("NO");
+  }
+}
